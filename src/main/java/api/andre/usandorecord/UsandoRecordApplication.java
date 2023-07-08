9ea11c185659e763d4e9664d36e5fbc3e8f81b37
André Victor <andrehsvictor@gmail.com>
2023-07-08 16:11:53 -0300

    feat: initial commit

---
+++ src/main/java/api/andre/usandorecord/UsandoRecordApplication.java
@@ -0,0 +1,13 @@
+package api.andre.usandorecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsandoRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsandoRecordApplication.class, args);
+	}
+
+}
